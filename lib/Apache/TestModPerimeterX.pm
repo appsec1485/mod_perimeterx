b72edaafdfad8f43b039ce8f0932f6c88cc6e2a1
Aviad Shikloshi <aviad@perimeterx.com>
2017-03-01 16:53:29 +0200

    test utils

---
+++ lib/Apache/TestModPerimeterX.pm
@@ -0,0 +1,47 @@
+package Apache::TestModPerimeterX;
+
+$Apache::TestModPerimeterX::Version = '0.1';
+
+use strict;
+use warnings;
+
+sub _bake_cookie {
+    my $self = shift;
+
+    use Crypt::KeyDerivation 'pbkdf2';
+    use Crypt::Misc 'encode_b64', 'decode_b64';
+    use Crypt::Mac::HMAC 'hmac_hex';
+    use Crypt::Mode::CBC;
+
+    my ( $ip, $ua, $score, $uuid, $vid, $time ) = @_;
+    my $data = $time . '0' . $score . $uuid . $vid . $ip . $ua;
+
+    my $password        = "perimeterx";
+    my $salt            = '12345678123456781234567812345678';
+    my $iteration_count = 1000;
+    my $hash_name       = undef;                              #default is SHA256
+    my $len             = 48;
+
+    my $km = pbkdf2( $password, $salt, $iteration_count, $hash_name, $len );
+    my $key = substr( $km, 0,  32 );
+    my $iv  = substr( $km, 32, 48 );
+
+    my $m         = Crypt::Mode::CBC->new('AES');
+    my $hmac      = hmac_hex( 'SHA256', $password, $data );
+    my $plaintext = '{"t":'
+    . $time
+    . ', "s":{"b":'
+    . $score
+    . ', "a":0}, "u":"'
+    . $uuid
+    . '", "v":"'
+    . $vid
+    . '", "h":"'
+    . $hmac . '"}';
+    my $ciphertext = $m->encrypt( $plaintext, $key, $iv );
+
+    my $cookie = encode_b64($salt) . ":" . 1000 . ":" . encode_b64($ciphertext);
+    return 'Cookie: _px=' . $cookie;
+};
+
+1;
--- t/001-sanity.t
+++ t/001-sanity.t
@@ -3,7 +3,15 @@ use warnings FATAL => 'all';
 
 use Apache::Test;
 use Apache::TestUtil;
+use Apache::TestModPerimeterX;
 
 plan tests => 1;
 
+my $cookie_baker = Apache::TestModPerimeterX->new;
+my $cookie = $cookie_baker->cookie_baker;
+print "cookie: $cookie\n";
+#print "what is this: $cookie_baker\n";
+
+#my $cookie = $test_utils->bake_cookie()
+#print "$cookie\n";
 ok 1;
