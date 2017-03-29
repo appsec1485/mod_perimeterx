ba0dbe3294ed65cc91b923d795a29c2e8290af6c
Aviad Shikloshi <aviad@perimeterx.com>
2017-03-29 18:02:07 +0300

    cookie v3 WIP

--- src/px_cookie_v3_decoder.h
+++ src/px_cookie_v3_decoder.h
@@ -1,7 +1,7 @@
 #ifndef PX_COOKIE_V3_H
 #define PX_COOKIE_V3_H
 
-int parse_cookie(const char *px_cookie, char* cookie_parts, int argc);
+int parse_cookie(const char *px_cookie, char **cookie_parts, int argc);
 
 
 #endif
